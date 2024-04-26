834273527fb79fd4662072bbc2e5093a249908be
João Gouveia <joaotalonegouveia@tecnico.ulisboa.pt>
2024-04-26 19:03:19 +0100

    More org configuration

--- hooks.el
+++ hooks.el
@@ -35,3 +35,20 @@
     indentation
     missing-newline-at-eof))
   (add-to-list 'write-file-functions 'delete-trailing-whitespace))
+
+;; org mode settings (strongly inspired by SystemCrafter's config)
+(defun jg/org-hook ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (auto-fill-mode 0)
+  (visual-line-mode 1)
+  (setq evil-auto-indent nil))
+
+;; auto tangle config file on save (strongly inspired by SystemCrafter's config)
+(defun jg/org-tangle-hook()
+  (when (string-equal (buffer-file-name)
+                      (expand-file-name "~/emacs.rc/config.org"))
+    ;; Dynamic scoping to the rescue
+    (let ((org-confirm-babel-evaluate nil))
+      (org-babel-tangle))))
+
