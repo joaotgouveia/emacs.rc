bc17bd7710e6c77eccf01c1ad067c0c7334c3b44
João Gouveia <joaotalonegouveia@tecnico.ulisboa.pt>
2024-04-29 09:38:03 +0100

    Remove neotree

--- hooks.el
+++ hooks.el
@@ -11,18 +11,6 @@
                    term-mode))
     (add-to-list 'evil-emacs-state-modes mode)))
 
-;; making neotree work with evil mode
-(defun jg/neotree-hook ()
-  (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter)
-  (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-quick-look)
-  (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
-  (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)
-  (define-key evil-normal-state-local-map (kbd "g") 'neotree-refresh)
-  (define-key evil-normal-state-local-map (kbd "n") 'neotree-next-line)
-  (define-key evil-normal-state-local-map (kbd "p") 'neotree-previous-line)
-  (define-key evil-normal-state-local-map (kbd "H") 'neotree-hidden-file-toggle)
-  (define-key evil-normal-state-local-map (kbd "A") 'neotree-stretch-toggle))
-
 ;; whitespace handling (strongly inspired by tsoding's config)
 (defun jg/whitespaces-hook ()
   (whitespace-mode t)
