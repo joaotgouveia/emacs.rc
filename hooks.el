de658a232936a752aea4d58fe19a6b45181ae213
João Gouveia <joaotalonegouveia@tecnico.ulisboa.pt>
2024-05-12 00:08:43 +0100

    Narrower visual column on org files

--- hooks.el
+++ hooks.el
@@ -54,7 +54,7 @@
 
 (defun jg/org-visual-fill-hook ()
   "Center text on screen."
-  (setq visual-fill-column-width 125
+  (setq visual-fill-column-width 75
         visual-fill-column-center-text t)
   (visual-fill-column-mode 1))
 
